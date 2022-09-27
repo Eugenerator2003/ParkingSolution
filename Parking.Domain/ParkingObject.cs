6ab46753ae2f0deed2df3281c14767bfb7a7341f
Гузов Евгений <eugeneguzov@mail.ru>
2022-09-27 14:29:31 +0300

    Добавьте файлы проекта.

---
+++ Parking.Domain/ParkingObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Domain
+{
+    internal abstract class ParkingObject
+    {
+        public int Id { get; set; }
+    }
+}
