bdcabc26933fbd2641273766769337ce640cbf38
Гузов Евгений <eugeneguzov@mail.ru>
2022-10-15 00:07:55 +0300

    Added laboratory work files

--- Parling.Application/ParkingContext.cs
+++ Parling.Application/ParkingContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using Parking.Domain;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Parking.Domain
+namespace Parking.Application
 {
     public partial class ParkingContext : DbContext
     {
@@ -29,8 +30,7 @@ namespace Parking.Domain
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-SRISG2B\\SQLEXPRESS;Database=Parking;Trusted_Connection=True;");
+               optionsBuilder.UseSqlServer(Connection.GetConnectionString());
             }
         }
 
