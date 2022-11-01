b6e239304a01a96000e966e13647637905f40606
Гузов Евгений <eugeneguzov@mail.ru>
2022-11-01 19:25:58 +0300

    Added generic cached interface and class

---
+++ WebApplication/Services/ICachedService.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Services
+{
+    public interface ICachedService<T> where T : class
+    {
+        void AddItem(string cacheKey);
+        IEnumerable<T> GetItems();
+        IEnumerable<T> GetItems(string cacheKey);
+    }
+}
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,49 +1,12 @@
-﻿namespace WebApplication
+﻿using WebApplication.Services;
+
+namespace WebApplication
 {
     public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-        }
-        
+    {      
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.Map("/Home", UseRun);
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Run writting");
-            });
             
-            
-        }
-
-        private static void UseRun(IApplicationBuilder app)
-        {
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Use writting");
-                await next.Invoke();
-            }
-            );
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("\nRun writting");
-            });
         }
     }
 }
